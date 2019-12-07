e04f60c55d2ba886de9300d3acdfde7282bc7e59
HP <khalilabubaker33@gmail.com>
2019-12-07 23:59:16 +0200

    khalil update 7/12/2019

--- app/src/main/java/com/example/eatmeatproject/MainActivity.java
+++ app/src/main/java/com/example/eatmeatproject/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_layout);
+        setContentView(R.layout.call_us);
      /*   handler=new Handler();
         handler.postDelayed(new Runnable() {
             @Override
