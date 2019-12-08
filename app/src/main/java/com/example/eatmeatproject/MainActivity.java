3680ffab3caa978496cf2e0278cd5f7c0648592c
HP <khalilabubaker33@gmail.com>
2019-12-08 12:59:23 +0200

    khalil update-3 8/12/2019

--- app/src/main/java/com/example/eatmeatproject/MainActivity.java
+++ app/src/main/java/com/example/eatmeatproject/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.delivery_address);
+        setContentView(R.layout.add_address);
      /*   handler=new Handler();
         handler.postDelayed(new Runnable() {
             @Override
