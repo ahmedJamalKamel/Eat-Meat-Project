b3a2106bb3ffbc20969f00a7a3587c64aa9a3099
HP <khalilabubaker33@gmail.com>
2019-12-10 15:26:08 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/example/eatmeatproject/MapActivity.java
@@ -0,0 +1,27 @@
+package com.example.eatmeatproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap=googleMap;
+    }
+}
