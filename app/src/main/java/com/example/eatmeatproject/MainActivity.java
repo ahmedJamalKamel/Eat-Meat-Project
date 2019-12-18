b6cfa7b91322ba2199f31f288f5d6036fff679f8
ahmedJamalKamel <ahmad.jamal.kamel@gmail.com>
2019-12-18 04:32:40 +0200

    FireBaseUi

--- app/src/main/java/com/example/eatmeatproject/MainActivity.java
+++ app/src/main/java/com/example/eatmeatproject/MainActivity.java
@@ -18,8 +18,16 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -28,10 +36,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     TextView textView;
     ImageView imageView_location;
     DrawerLayout drawer;
+    private static final int RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        showLoginUi();
         setContentView(R.layout.side_navigation);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         viewPager = findViewById(R.id.view_pager1);
@@ -144,7 +154,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.setting:
                 break;
             case R.id.exit:
-
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                recreate();
+                            }
+                        });
                 break;
         }
 
@@ -175,6 +192,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         @Override
         public int getCount() {
             return 4;
+        }
+    }
+
+    private void showLoginUi() {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                    new AuthUI.IdpConfig.FacebookBuilder().build(),
+                    new AuthUI.IdpConfig.AnonymousBuilder().build()
+            );
+            startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder().setIsSmartLockEnabled(false).setAvailableProviders(providers).build(),
+                    RC_SIGN_IN
+            );
         }
     }
 
