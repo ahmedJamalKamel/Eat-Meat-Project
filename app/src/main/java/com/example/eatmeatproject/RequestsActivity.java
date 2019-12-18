d1b36e36d31d57a0445553c598a1cdbe1967a25f
HP <khalilabubaker33@gmail.com>
2019-12-18 13:30:07 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/eatmeatproject/RequestsActivity.java
+++ app/src/main/java/com/example/eatmeatproject/RequestsActivity.java
@@ -1,14 +1,23 @@
 package com.example.eatmeatproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class RequestsActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,8 +29,46 @@ public class RequestsActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(RequestsActivity.this, MainActivity.class));
+                onBackPressed();
             }
         });
+
+        recyclerView=findViewById(R.id.request_recyclerview);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this,RecyclerView.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        RequestAdapter adapter = new RequestAdapter(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.ViewHolder> {
+        Context context;
+        public RequestAdapter(Context context) {
+         this.context=context;
+        }
+
+        @NonNull
+        @Override
+        public RequestAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.request_item, parent, false);
+            return new RequestAdapter.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RequestAdapter.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+        }
+
+
     }
 }
